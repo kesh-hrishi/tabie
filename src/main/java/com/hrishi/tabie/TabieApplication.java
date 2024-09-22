576031e5dd326097d08e83518bdb871f82efdaf7
hrishikesh <hrishikeshajith0@gmail.com>
2024-09-23 01:27:16 +0530

    Initial commit

---
+++ src/main/java/com/hrishi/tabie/TabieApplication.java
@@ -0,0 +1,13 @@
+package com.hrishi.tabie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrishi/tabie/TabieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrishi.tabie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
