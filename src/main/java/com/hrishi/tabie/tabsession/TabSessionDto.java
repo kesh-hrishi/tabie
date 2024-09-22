becb17570e5488f374dc8602b8b0df9cb218a328
hrishikesh <hrishikeshajith0@gmail.com>
2024-09-23 01:44:03 +0530

    added TabSessionDto and TabSessionRepository

---
+++ src/main/java/com/hrishi/tabie/tabsession/TabSessionDto.java
@@ -0,0 +1,4 @@
+package com.hrishi.tabie.tabsession;
+
+public record TabSessionDto(String sessionName,Integer userId) {
+}
---
+++ src/main/java/com/hrishi/tabie/tabsession/TabSessionRepository.java
@@ -0,0 +1,6 @@
+package com.hrishi.tabie.tabsession;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TabSessionRepository extends JpaRepository<TabSession, Integer> {
+}
