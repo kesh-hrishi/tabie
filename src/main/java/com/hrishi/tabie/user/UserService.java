7f58daf035c2056a1af43c4e7845cd7782f1e222
hrishikesh <hrishikeshajith0@gmail.com>
2024-09-24 21:12:08 +0530

    added UserService

---
+++ src/main/java/com/hrishi/tabie/user/UserService.java
@@ -0,0 +1,39 @@
+package com.hrishi.tabie.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    public UserResponseDtoWithoutRelations saveUser(UserDto userDto) {
+        var user = userMapper.toUser(userDto);
+        var savedUser = userRepository.save(user);
+        return userMapper.touserResponseDtoWithoutRelations(savedUser);
+    }
+
+    public List<UserResponseDto> displayUser() {
+        return userRepository.findAll().stream()
+                .map(userMapper::toUserResponseDto)
+                .collect(Collectors.toList());
+    }
+
+    public UserResponseDto displayUserById(Integer userId) {
+        return userRepository.findById(userId)
+                .map(userMapper::toUserResponseDto)
+                .orElse(null);
+    }
+
+    public void deleteUserById(Integer userId) {
+        userRepository.deleteById(userId);
+    }
+}
