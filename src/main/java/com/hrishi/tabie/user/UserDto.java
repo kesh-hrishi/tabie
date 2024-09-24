4f7fe343c3c1e18b5f9f030a53e19c606e1f225f
hrishikesh <hrishikeshajith0@gmail.com>
2024-09-24 13:32:00 +0530

    added User Dto and User response Dto

---
+++ src/main/java/com/hrishi/tabie/user/UserDto.java
@@ -0,0 +1,4 @@
+package com.hrishi.tabie.user;
+
+public record UserDto(String userName,String email,String password) {
+}
---
+++ src/main/java/com/hrishi/tabie/user/UserResponseDto.java
@@ -0,0 +1,4 @@
+package com.hrishi.tabie.user;
+
+public record UserResponseDto(String userName,String email,Integer tabSessionId) {
+}
