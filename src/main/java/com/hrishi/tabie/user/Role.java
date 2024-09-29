3a376396868b9ded571e4b1fec4103dcd0578e9b
hrishikesh <hrishikeshajith0@gmail.com>
2024-09-30 01:39:57 +0530

    implemented UserDetails interface to our custom User class

---
+++ src/main/java/com/hrishi/tabie/user/Role.java
@@ -0,0 +1,6 @@
+package com.hrishi.tabie.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/hrishi/tabie/user/User.java
+++ src/main/java/com/hrishi/tabie/user/User.java
@@ -3,24 +3,31 @@ package com.hrishi.tabie.user;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.hrishi.tabie.tabsession.TabSession;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "T_USER")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
     private Integer id;
 
-    @Column(unique = true)
-    private String userName;
+    private String username;
 
     @Column(unique = true)
     private String email;
@@ -31,17 +38,45 @@ public class User {
 
     private LocalDateTime updatedAt;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @OneToMany(
             mappedBy = "user"
     )
     @JsonManagedReference
     private List<TabSession> tabSessions;
 
-    public User(String userName, String email, String password, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.userName = userName;
+    public User(String username, String email, String password, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.username = username;
         this.email = email;
         this.password = password;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
